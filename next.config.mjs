78b1a876ab4af193691a2041a75d886677606c63
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2026-01-14 08:11:16 +0530

    Updated to auto deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   eslint: {
     ignoreDuringBuilds: true,
   },
